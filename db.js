5b0c95a302286a1e9dcd2ff087fa1d77df11a981
mahamraani47-ui <mahamraani47@gmail.com>
2026-09-18 11:15:03 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,11 @@
 
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+async function connectToDatabase() {
+  await client.connect();
+  return client.db("giftlink");
+}
+
+module.exports = { connectToDatabase };
